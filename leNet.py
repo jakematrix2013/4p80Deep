9608ab01a7a74c0d8e546687d94e82b3954d8807
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2023-05-03 17:19:39 -0400

    Created GUI to draw numbers

--- leNet.py
+++ leNet.py
@@ -3,6 +3,10 @@ from keras import models, layers
 from keras.datasets import mnist
 from keras.utils import np_utils
 import matplotlib.pyplot as plt
+from tkinter import *
+from tkinter.ttk import Scale
+from tkinter import colorchooser, filedialog, messagebox
+import PIL.ImageGrab as ImageGrab
 
 class LeNet:
 
@@ -66,4 +70,63 @@ class LeNet:
 
         plt.show()
 
-leNet = LeNet(10)
+class Draw():
+    def __init__(self, root, title):
+        self.root = root
+        self.root.title(title)
+        self.root.geometry('300x330')
+        self.root.configure(background='white')
+        self.root.resizable(0, 0)
+
+        # variables for pointer and Eraser   
+        self.pointer = "black"
+        self.erase = "white"
+
+        # Draw Button
+        self.drawer_btn = Button(self.root, text='Draw', bd=4, bg='green', relief=RIDGE, width=9, command=lambda col='black' : self.select_color(col))
+        self.drawer_btn.place(x=5, y=295)
+
+        # Erase Button
+        self.eraser_btn = Button(self.root, text="Eraser", bd=4, bg='green', command=self.eraser, width=9, relief=RIDGE)
+        self.eraser_btn.place(x=105, y=295)
+
+        # Reset Button to clear the entire screen 
+        self.clear_screen= Button(self.root, text="Clear Screen", bd=4, bg='green', command=lambda : self.background.delete('all'), width=9, relief=RIDGE)
+        self.clear_screen.place(x=215, y=295)
+
+        # Creating the pointer
+        self.pointer_frame = LabelFrame(self.root, text='size', bd=5, bg='white', font=('arial', 15, 'bold'), relief=RIDGE)
+        self.pointer_size = Scale(self.pointer_frame, orient=VERTICAL, from_=48, to=0, length=168)
+        self.pointer_size.set(1)
+
+        # Defining a background color for the Canvas 
+        self.background = Canvas(self.root, bg='white', bd=5, relief=GROOVE, height=280, width=280)
+        self.background.place(x=10, y=0)
+
+        #Bind the background Canvas with mouse click
+        self.background.bind("<B1-Motion>", self.paint)
+
+    # Paint Function for Drawing the lines on Canvas
+    def paint(self, event):
+        x1, y1 = (event.x - 2), (event.y - 2)
+        x2, y2 = (event.x + 2), (event.y + 2)
+
+        self.background.create_oval(x1, y1, x2, y2, fill=self.pointer, outline=self.pointer, width=self.pointer_size.get())
+
+    def select_color(self,col):
+        self.pointer = col
+
+    # Function for defining the eraser
+    def eraser(self):
+        self.pointer= self.erase
+
+def main():
+    # leNet = LeNet(10)
+
+    root = Tk()
+    p = Draw(root, 'Canvas')
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
