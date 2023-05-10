1b4fa7077c8a5bf6bd68018a8036d78077e41e46
Abhijeet Prajapati <abhijeetprajapati85@gmail.com>
2023-05-10 09:06:27 -0400

    Add option to allow user to train their own model

--- Project.py
+++ Project.py
@@ -1,13 +1,27 @@
+from LeNet import LeNet
 from GUI import Draw
 import tkinter as tk
 
 def main():
+    ch = None
     modelFilePath = './4p80Deep/Model/'
-    # leNet = LeNet(10, modelFilePath)
 
-    root = tk.Tk()
-    p = Draw(root, 'Number Canvas', modelFilePath)
-    root.mainloop()
+    while (ch != '3'):
+        ch = input('1: Train new model\n2: Test current Model\n3: Exit\n> ')
+
+        if ch == '1':
+            try:
+                numEpochs = int(input('Number of epochs to train for: '))
+                
+                leNet = LeNet(numEpochs, modelFilePath)
+
+            except:
+                print('Integers only.')
+
+        elif ch == '2':
+            root = tk.Tk()
+            p = Draw(root, 'Number Canvas', modelFilePath)
+            root.mainloop()
     
 
 if __name__ == '__main__':
